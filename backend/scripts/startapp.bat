526ddff20dd7ef9e848a60b4a5ab3b766d551489
骑士姬 <2067834160@qq.com>
2025-11-30 09:57:51 +0800

    feat: 添加 app 创建模板

--- backend/scripts/startapp.bat
+++ backend/scripts/startapp.bat
@@ -1,2 +1,2 @@
 cd "../"
-start cmd /k .\.venv\Scripts\python.exe .\manage.py startapp %1
+start cmd /k .\.venv\Scripts\python.exe .\manage.py startapp %1 --template=./templates/app_template
--- docker/djagon-backend.Dockerfile
+++ docker/djagon-backend.Dockerfile
@@ -3,9 +3,6 @@ FROM ghcr.io/astral-sh/uv:debian-slim
 # 设置工作目录
 WORKDIR /workspace
 
-# 下载 python
-RUN uv --no-cache python install 3.14
-
 # 复制环境配置
 COPY pyproject.toml uv.lock ./
 
